20343b764916ef28bee15f47becc61d1427f156d
samuEl-FS <samuEl-FS@users.noreply.github.com>
2020-09-29 22:37:24 +0530

    Code file created.

---
+++ sample/code.py
@@ -0,0 +1,20 @@
+# --------------
+# Code starts here
+
+# Assign variable names
+
+age = 12
+name = 'John'
+
+# Check types
+
+type_age = type(age)
+type_name = type(name)
+
+# print out types
+print(type_age)
+print(type_name)
+
+# Code ends here
+
+
